1afb7be8ff8f69e5025cec809e6a8a24a9a498e2
sickboyyy <sickboyyyyy@protonmail.com>
2021-02-08 21:19:51 +0100

    Sticking to standard indentation in test file

--- test_main.py
+++ test_main.py
@@ -6,13 +6,8 @@ client = TestClient(app)
 
 
 def run_test(mmr0, rd0, t1win, mmr1, rd1):
-    response = client.post(
-        "/mmr/update",
-        json={
-          "ratings_list": mmr0,
-          "rds_list": rd0,
-          "t1_won": t1win},
-    )
+    data = {"ratings_list": mmr0, "rds_list": rd0, "t1_won": t1win}
+    response = client.post("/mmr/update", json=data)
     assert response.status_code == 200
 
     result = response.json()
@@ -29,27 +24,26 @@ def run_test(mmr0, rd0, t1win, mmr1, rd1):
 
 def test_2x2():
     run_test(
-      [ 1400, 1600, 1340, 1700 ],
-      [ 350, 350, 350, 350 ],
-      True,
-      [1530, 1714, 1203, 1592],
-      [278, 278, 278, 278]
+        [1400, 1600, 1340, 1700],
+        [350, 350, 350, 350],
+        True,
+        [1530, 1714, 1203, 1592],
+        [278, 278, 278, 278]
     )
 
 
 def test_1x1():
     run_test(
-      [ 2000, 1500 ],
-      [ 90, 350 ],
-      True,
-      [2002, 1465],
-      [90, 318]
+        [2000, 1500],
+        [90, 350],
+        True,
+        [2002, 1465],
+        [90, 318]
     )
     run_test(
-      [ 2000, 1500 ],
-      [ 90, 350 ],
-      False,
-      [1966, 2021],
-      [90, 318]
+        [2000, 1500],
+        [90, 350],
+        False,
+        [1966, 2021],
+        [90, 318]
     )
-
